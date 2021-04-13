f8b3e57e113dc1eb81d88b7e6d2c534f28d77a50
Laila <lailacampbell96@icloud.com>
2021-04-13 03:14:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,3 +8,10 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
+
+def display_board(board)
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
