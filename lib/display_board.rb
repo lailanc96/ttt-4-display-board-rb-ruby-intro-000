3893cc4ed49ae6b32f7aea45628937fc173f5b5a
Laila <lailacampbell96@icloud.com>
2021-04-13 03:20:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,7 +9,7 @@ def display_board(board)
   puts "   |   |   "
 end
 
-def display_board(board = " "," "," "," ","X"," "," "," "," ",)
+def display_board(board = " "," "," "," "," "," "," "," "," ",)
 puts "   |   |   "
 puts "-----------"
 puts "   | X  |   "
