c9ec61b83b4c4ea6e33534fe98617fb441852413
Laila <lailacampbell96@icloud.com>
2021-04-13 03:25:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = " "," "," "," "," "," "," "," "," ",
+
 def display_board(board)
   #puts "   "
   puts "   |   |   "
@@ -7,4 +9,5 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  puts 
 end
